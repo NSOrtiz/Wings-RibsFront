af01678d8d47d6c0e9b811ab7cb119475b6b2fa9
Natalia Ortiz <nsol1094@gmail.com>
2024-09-07 08:28:26 -0600

    barra de navegacion, pruebas y equivalencias de color

---
+++ src/components/Colors.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import '../CSS/colors_text.css';
+
+export default function Colorstest() {
+  return (
+    <section className="bg-neutral-100 w-4/5 flex flex-col justify-center gap-4 p-4">
+      <h1 className="text-amber-50 title1 text-outline">
+        fondo: neutral-100 = GrisF
+      </h1>
+      <div className="bg-neutral-800 w-full flex justify-center flex-col p-2">
+        <h1 className="text-amber-50 title2">bg-box: neutral-800 = Gris950</h1>
+        <h2 className="text-amber-50 title2">texto1: amber-50 = Amarillo-50</h2>
+        <h2 className="text-amber-100 title2">
+          texto2: amber-100 = Amarillo-100
+        </h2>
+        <h2 className="text-amber-200 title2">
+          texto2: amber-200 = Amarillo-200
+        </h2>
+        <h2 className="text-amber-300 title2">
+          texto2: amber-300 = Amarillo-300
+        </h2>
+        <h2 className="text-amber-400 title2">
+          texto2: amber-400 = Amarillo-400
+        </h2>
+        <h2 className="text-amber-500 title2">
+          texto2: amber-500 = Amarillo-500
+        </h2>
+        <h2 className="text-amber-600 title2">
+          texto2: amber-600 = Amarillo-600
+        </h2>
+      </div>
+      <div className="bg-amber-500 w-full flex justify-center flex-col p-2">
+        <h1 className="text-neutral-100 title2">
+          bg-box: amber-500 = Amarillo-500
+        </h1>
+        <h2 className="text-neutral-100 title2">texto1: neutral-100 = Gris1</h2>
+        <h2 className="text-neutral-300 title2">
+          texto1: neutral-300 = Gris-300
+        </h2>
+        <h2 className="text-neutral-500 title2">
+          texto1: neutral-500 = Gris-500
+        </h2>
+        <h2 className="text-neutral-700 title2">
+          texto1: neutral-700 = Gris-800
+        </h2>
+        <h2 className="text-neutral-800 title2">
+          texto1: neutral-800 = Gris-950
+        </h2>
+      </div>
+      <h2 className="text-amber-50 title2">texto1: amber-50 = Amarillo-50</h2>
+      <h2 className="text-amber-100 title2">
+        texto2: amber-100 = Amarillo-100
+      </h2>
+      <h2 className="text-amber-200 title2">
+        texto2: amber-200 = Amarillo-200
+      </h2>
+      <h2 className="text-amber-300 title2">
+        texto2: amber-300 = Amarillo-300
+      </h2>
+      <h2 className="text-amber-400 title2">
+        texto2: amber-400 = Amarillo-400
+      </h2>
+      <h2 className="text-amber-500 title2">
+        texto2: amber-500 = Amarillo-500
+      </h2>
+      <h2 className="text-amber-600 title2">
+        texto2: amber-600 = Amarillo-600
+      </h2>
+      <h2 className="text-neutral-100 title2 text-outline">
+        texto1: neutral-100 = Gris1
+      </h2>
+      <h2 className="text-neutral-300 title2">
+        texto1: neutral-300 = Gris-300
+      </h2>
+      <h2 className="text-neutral-500 title2">
+        texto1: neutral-500 = Gris-500
+      </h2>
+      <h2 className="text-neutral-700 title2">
+        texto1: neutral-700 = Gris-800
+      </h2>
+      <h2 className="text-neutral-800 title2">
+        texto1: neutral-800 = Gris-950
+      </h2>
+    </section>
+  );
+}
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import NavBtn from './NavBtn';
+
+export default function NavBar() {
+  return (
+    <header className="w-full flex justify-stretch items-center py-2 px-[80px] bg-neutral-800 default-shadow">
+      <div className="flex justify-between items-center flex-1 self-stretch">
+        <div className="flex items-center gap-4 flex-1 text-">
+          <img
+            src="/images/W&R1.png"
+            alt="logo"
+            className="w-[44px] h-[44px] rounded-full cursor-pointer"
+          />
+          <h1 className="title1 text-amber-500">Wings & Ribs</h1>
+        </div>
+        <NavBtn />
+      </div>
+    </header>
+  );
+}
