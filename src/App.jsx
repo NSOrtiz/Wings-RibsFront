c78c57bdca17523c6bde532311ad5530c19b1fef
AnaisLaguna <annslaguna@gmail.com>
2024-09-18 16:43:30 -0600

    metodo de pago listo

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import ProfilePage from './pages/ProfilePage';
 import Menu1 from './pages/menusucursal';
+import MetodoPago from './pages/metodo_pago';
 import React from 'react';
+
 
 export default function App() {
   return (
@@ -14,6 +16,7 @@ export default function App() {
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/home/menu" element={<Menu1 />} />
+        <Route path="/metodo_pago" element={<MetodoPago />} />
       </Routes>
     </BrowserRouter>
   );
