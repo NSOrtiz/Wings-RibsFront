9adb5aa9bbaa8e77399b117e5313549413292718
patrick espinoza <patrickespinoza38@gmail.com>
2024-09-17 22:01:44 -0600

    cambio de texto y color

--- src/pages/Confirmacion-com.jsx
+++ src/pages/Confirmacion-com.jsx
@@ -9,55 +9,67 @@ function Confirmacioncom() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-neutral-200 p-4">
       <div className="p-6 max-w-7xl w-full rounded-lg shadow-md">
-        <h1 className="text-3xl font-bold mb-10 text-center text-amber-500">Confirmar compra</h1>
+        <h1 id='ubuntu-bold' className="text-[32px] mb-10 text-center text-amber-500">Confirmar compra</h1>
         <div className="flex flex-col lg:flex-row gap-4">
           
           <div className="flex-1 flex flex-col">
             <div className="flex flex-col items-center bg-white p-4 rounded-xl">
               <div className="flex items-center pb-2 w-full">
-                <h2 className="text-lg font-semibold flex-1 text-center text-amber-500">No. de pedido</h2>
-                <img src="/edit_24.svg" alt="Edit" className="w-6 h-6 ml-4" />
+                <h2 id='ubuntu-bold' className="text-[16px] flex-1 text-center text-amber-500">No. de pedido</h2>
               </div>
               <div className="flex flex-col space-y-4 mt-4 w-full">
                 {[1, 2, 3].map((item, index) => (
-                  <div key={index} className="flex items-center border-b border-neutral-200 pb-2 w-full justify-between">
-                    <div className="flex items-center">
-                      <p className="mr-2 text-amber-500">1</p>
-                      <img src="/edit_24.svg" alt="Edit" className="w-6 h-6 text-amber-500" />
+                  <div key={index} className="flex flex-col border-b border-amber-300 pb-2 w-full">
+                    <div className="flex items-center justify-start">
+                      <img src="/images/chickenplate.png" alt="Plato" className="w-[70px] h-[60px]" />
+                      <div className='flex flex-col mx-8'>
+                        <h2 id='ubuntu-bold' className="text-[16px]">Platillo 1</h2>
+                        <p id='ubuntu-light' className='text-[12px]'>Detalle del producto</p>
+                        <p id='ubuntu-medium' className='text-[16px]'>MX $210</p>
+                      </div>
                     </div>
-                    <div className='w-60 flex flex-col mx-4'>
-                      <h2 className="text-lg font-semibold ">Plato de ostras</h2>
-                      <p className='font-extralight'>Galletas de chocolate con forma de tortuga y red velvet</p>
+                    
+                    <div className="flex items-center mt-2 justify-between">
+                      <div className="flex items-center">
+                        <img src="/icons/trash-outline.svg" alt="Eliminar" className="w-4 h-4 mr-1" />
+                        <button className="mr-2">Eliminar</button>
+                      </div>
+                      <div className="flex items-center">
+                        <img src="/icons/add-amber.svg" alt="añadir" className="w-6 h-6 mr-1 rounded-full border border-amber-400" />
+                        <p className="mr-1 text-[16px]">1</p>
+                        <img src="/icons/remove-amber.svg" alt="" className="w-6 h-6 rounded-full border border-amber-400" />
+                      </div>
                     </div>
-                    <p className="text-amber-500">$10.00 mxn</p> 
+                    <div className="flex items-center mt-2 justify-end">
+                      <div className="flex items-center">
+                        <p id='ubuntu-light' className="mr-2 ">1 producto x</p>
+                        <p id='ubuntu-medium' className="mr-1 "> MX $10.00</p>
+                      </div>
+                    </div>
                   </div>
                 ))}
                 <div className="flex flex-col space-y-4 mt-4 w-full">
                   <div className="flex justify-between pb-2">
-                    <p className="font-extralight w-1/3">Subtotal</p>
-                    <p className="w-2/3 text-right text-amber-500">$30.00 mxn</p> 
+                    <p id='ubuntu-light' className="text-[16px]">Subtotal</p>
+                    <p id='ubuntu-medium' className="text-[16px] text-amber-500">$30.00 mxn</p> 
                   </div>
                   <div className="flex justify-between pb-2">
-                    <p className="font-extralight w-1/3">Descuento</p>
-                    <p className="w-2/3 text-right text-amber-500">$10.00 mxn</p> 
+                    <p id='ubuntu-light' className="text-[16px]">Descuento</p>
+                    <p id='ubuntu-medium' className="text-[16px] text-amber-500">$10.00 mxn</p> 
                   </div>
                   <div className="flex justify-between pb-2">
-                    <p className="font-bold w-1/3">Total</p>
-                    <p className="w-2/3 text-right text-2xl font-bold text-amber-500">$220.00 mxn</p> 
-                  </div>
-                  <div className="flex justify-end pb-2">
-                    <p className="w-2/3 text-right text-[#1BB816]">obtienes 20 pts. en esta compra</p>
+                    <p id='ubuntu-medium' className="text-[28px]">Total</p>
+                    <p id='ubuntu-medium' className="text-[28px] text-amber-500">$220.00 mxn</p> 
                   </div>
                 </div>
               </div>
             </div>
           </div>
 
-       
           <div className="flex-1 flex flex-col lg:mt-0 mt-4">
-            <div className="flex flex-col items-center bg-white p-4 rounded-xl">
+            <div className="flex flex-col items-center bg-white p-4 rounded-xl h-full">
               <div className="flex items-center pb-2 w-full relative">
-                <h2 className="text-lg font-semibold flex-1 text-center">Datos del cliente</h2>
+                <h2 id='ubuntu-bold' className="text-[16px] flex-1 text-center">Datos del cliente</h2>
                 <img
                   src="/edit_24.svg"
                   alt="Edit"
@@ -67,12 +79,12 @@ function Confirmacioncom() {
                 {isVisible && (
                   <div className='fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 z-50'>
                     <div className='bg-slate-200 p-6 shadow-lg w-full max-w-md rounded-lg'>
-                      <h2 className='py-2 text-xl font-semibold'>Nombre</h2>
+                      <h2 id='ubuntu-regular' className='py-2 text-[28px]'>Nombre</h2>
                       <input type="text" placeholder='Escribe tu nombre' className='border border-neutral-500 w-full py-3 rounded-md'/>
-                      <h2 className='py-2 text-xl font-semibold mt-4'>Numero telefonico</h2>
-                      <input type="text" placeholder='Phone number' className='border border-neutral-500 w-full py-3 rounded-md' />
-                      <h2 className='py-2 text-xl font-semibold mt-4'>Direccion</h2>
-                      <input type="text" placeholder='Write Address' className='border border-neutral-500 w-full py-3 rounded-md'/>
+                      <h2 id='ubuntu-regular' className='py-2 text-[28px]' >Numero telèfonico</h2>
+                      <input type="text" placeholder='Numero telèfonico' className='border border-neutral-500 w-full py-3 rounded-md' />
+                      <h2 id='ubuntu-regular' className='py-2 text-[28px]' >Direcciòn</h2>
+                      <input type="text" placeholder='Escribe tu direcciòn' className='border border-neutral-500 w-full py-3 rounded-md'/>
                       <div className='mt-6 flex justify-between'>
                         <button
                           className="px-6 py-2 bg-amber-500 text-white rounded-md hover:bg-amber-600"
@@ -92,31 +104,30 @@ function Confirmacioncom() {
               </div>
               <div className="flex flex-col space-y-4 mt-4 w-full">
                 <div className="flex justify-between border-b border-neutral-200 pb-2">
-                  <p className="font-semibold w-1/3">Nombre</p>
-                  <p className="w-2/3 text-right">Albert Stevano</p>
+                  <p id='ubuntu-light' className="text-[16px]">Nombre</p>
+                  <p id='ubuntu-regular' className="text-[16px]">Albert Stevano</p>
                 </div>
                 <div className="flex justify-between border-b border-neutral-200 pb-2">
-                  <p className="font-semibold w-1/3">Direccion</p>
-                  <p className="w-2/3 text-right">ygahysisysi</p>
+                  <p id='ubuntu-light' className="text-[16px]">Direccion</p>
+                  <p id='ubuntu-regular' className="text-[16px]">ygahysisysi</p>
                 </div>
                 <div className="flex justify-between border-b border-neutral-200 pb-2">
-                  <p className="font-semibold w-1/3">Codigo postal</p>
-                  <p className="w-2/3 text-right">BCBSBER</p>
+                  <p id='ubuntu-light' className="text-[16px]">Codigo postal</p>
+                  <p id='ubuntu-regular' className="text-[16px]">BCBSBER</p>
                 </div>
                 <div className="flex justify-between pb-2">
-                  <p className="font-semibold w-1/3">Numero telefonico</p>
-                  <p className="w-2/3 text-right">+12 83472838 28</p>
+                  <p id='ubuntu-light' className="text-[16px]">Numero telefonico</p>
+                  <p id='ubuntu-regular' className="text-[16px]">+12 83472838 28</p>
                 </div>
+              </div>
+             
+              <div className="mt-auto flex justify-center w-full">
+                <button className="px-20 py-2 bg-amber-500 text-white rounded-xl hover:bg-amber-600">
+                  Enviar
+                </button>
               </div>
             </div>
           </div>
-        </div>
-
-        
-        <div className="flex justify-center mt-6">
-          <button className="px-4 py-2 bg-amber-500 text-white rounded-xl hover:bg-amber-600">
-            Confirmar compra
-          </button>
         </div>
       </div>
     </div>
@@ -124,3 +135,5 @@ function Confirmacioncom() {
 }
 
 export default Confirmacioncom;
+
+
--- src/pages/Estatus-de-pedido.jsx
+++ src/pages/Estatus-de-pedido.jsx
@@ -16,47 +16,47 @@ function EstatusDePedido() {
                   <div key={index} className="flex items-center border-b border-neutral-200 pb-2 w-full justify-between">
                     <div className="flex items-center">
                       <p className="mr-2 text-amber-500">1</p>
-                      <img src="/edit_24.svg" alt="Edit" className="w-6 h-6 text-amber-500" />
+                      <img src="/images/Rectangle-19.png" alt="Edit" className="w-[65px] h-[82px] p-2 text-amber-500" />
                     </div>
                     <div className='w-60 flex flex-col mx-4'>
-                      <h2 className="text-lg font-semibold ">Plato de ostras</h2>
-                      <p className='font-extralight'>Galletas de chocolate con forma de tortuga y red velvet</p>
+                      <h2 id='ubuntu-bold' className="text-[16px]" >Plato de ostras</h2>
+                      <p id='ubuntu-light' className='text-[12px]'>Galletas de chocolate con forma de tortuga y red velvet</p>
                     </div>
-                    <p className="text-amber-500">$10.00 mxn</p> 
+                    <p id='ubuntu-medium' className="text-[16px] text-amber-500">$10.00 mxn</p> 
                   </div>
                 ))}
                 <div className="flex flex-col space-y-4 mt-4 w-full">
                   <div className="flex justify-between pb-2">
-                    <p className="font-extralight text-sm md:text-base w-1/3">Subtotal</p>
-                    <p className="text-sm md:text-base w-2/3 text-right text-amber-500">$30.00 mxn</p> 
+                    <p d='ubuntu-light' className="text-[16px]">Subtotal</p>
+                    <p id='ubuntu-medium' className="text-[16px] text-amber-500">$30.00 mxn</p> 
                   </div>
                   <div className="flex justify-between pb-2">
-                    <p className="font-extralight text-sm md:text-base w-1/3">Descuento</p>
-                    <p className="text-sm md:text-base w-2/3 text-right text-amber-500">$10.00 mxn</p> 
+                    <p d='ubuntu-light' className="text-[16px]">Descuento</p>
+                    <p id='ubuntu-medium' className="text-[16px] text-amber-500">$10.00 mxn</p> 
                   </div>
                   <div className="flex justify-between pb-2">
-                    <p className="font-bold text-sm md:text-base w-1/3">Total</p>
-                    <p className="text-sm md:text-base w-2/3 text-right font-bold text-amber-500">$220.00 mxn</p> 
+                    <p id='ubuntu-medium' className="text-[28px]">Total</p>
+                    <p id='ubuntu-medium' className="text-[28px] text-amber-500">$220.00 mxn</p> 
                   </div>
                   <div className="flex items-center pb-2 w-full relative">
-                    <h2 className="text-base md:text-lg font-semibold flex-1 text-center">Datos del cliente</h2>
+                    <h2 id='ubuntu-bold' className="text-[16px] flex-1 text-center">Datos del cliente</h2>
                   </div>
                   <div className="flex flex-col space-y-4 mt-4 w-full">
                     <div className="flex justify-between border-b border-neutral-200 pb-2">
-                      <p className="font-semibold text-sm md:text-base w-1/3">Nombre</p>
-                      <p className="text-sm md:text-base w-2/3 text-right">Albert Stevano</p>
+                      <p id='ubuntu-light' className="text-[16px]">Nombre</p>
+                      <p id='ubuntu-regular' className="text-[16px]">Albert Stevano</p>
                     </div>
                     <div className="flex justify-between border-b border-neutral-200 pb-2">
-                      <p className="font-semibold text-sm md:text-base w-1/3">Direccion</p>
-                      <p className="text-sm md:text-base w-2/3 text-right">ygahysisysi</p>
+                      <p id='ubuntu-light' className="text-[16px]">Direccion</p>
+                      <p id='ubuntu-regular' className="text-[16px]">ygahysisysi</p>
                     </div>
                     <div className="flex justify-between border-b border-neutral-200 pb-2">
-                      <p className="font-semibold text-sm md:text-base w-1/3">Codigo Postal</p>
-                      <p className="text-sm md:text-base w-2/3 text-right">BCBSBER</p>
+                      <p id='ubuntu-light' className="text-[16px]">Codigo Postal</p>
+                      <p id='ubuntu-regular' className="text-[16px]">BCBSBER</p>
                     </div>
                     <div className="flex justify-between pb-2">
-                      <p className="font-semibold text-sm md:text-base w-1/3">Numero telefonico</p>
-                      <p className="text-sm md:text-base w-2/3 text-right">+12 83472838 28</p>
+                      <p id='ubuntu-light' className="text-[16px]">Numero telefonico</p>
+                      <p id='ubuntu-regular' className="text-[16px]">+12 83472838 28</p>
                     </div>
                   </div>
                 </div>
@@ -66,22 +66,22 @@ function EstatusDePedido() {
 
           <div className="flex-1 flex flex-col lg:flex-col md:flex-col space-y-4 items-center justify-center">
             <div className="text-center mb-6">
-              <h2 className="text-2xl md:text-3xl font-semibold">Estatus de pedido</h2>
-              <p className='text-black/40 text-sm md:text-base'>Hora estimada de entrega 8:30 - 9:15 PM</p>
+              <h2 id='ubuntu-bold' className="text-[32px] md:text-[32px]">Estatus de pedido</h2>
+              <p  id='ubuntu-light' className='text-[28px] md:text-[28px]'>Hora estimada de entrega 8:30 - 9:15 PM</p>
             </div>
 
             <div className='flex flex-col lg:flex-row md:flex-row md:space-x4 md:space-y-0 lg:space-x-4 space-y-4 lg:space-y-0'>
               <div className="flex-1 text-center flex flex-col space-y-4">
-                <div className="flex flex-col items-center bg-amber-500 p-3 rounded-xl">
-                  <div className="flex flex-col space-y-4 mt-4 w-48 md:w-36 h-52 md:h-60">
-                    <div className="flex flex-col justify-between pb-2">
-                      <div className='flex items-center justify-center mb-4'>
-                        <img className='rounded-full w-20 md:w-15 h-20 md:h-20 object-cover' src="/icons/fastfood.svg" alt="Recibido" />
+                <div className="flex flex-col items-center bg-white p-3 rounded-xl">
+                  <div className="flex flex-col space-y-4 mt-4 w-48 md:w-32 h-52 md:h-52 ">
+                    <div className="flex flex-col items-center justify-between pb-2  ">
+                      <div className='flex items-center justify-center bg-amber-500 rounded-full w-[100px] h-[100px] md:w-15 md:h-15 overflow-hidden'>
+                        <img className='w-auto h-auto max-w-full max-h-full' src="/icons/Group.svg" alt="Recibido" />
                       </div>
-                      <p className="font-semibold text-sm md:text-base">Recibido</p>
-                      <p className='text-black/40 text-sm md:text-base'>8:30 am</p>
-                      <p className="text-black/40 text-sm md:text-base">Hemos recibido tu pedido</p>
-                      <h2 className='font-semibold text-sm md:text-base'>Sucursal Loma real</h2>
+                      <p id='ubuntu-medium' className="text-[16px] md:text-[16px]">Recibido</p>
+                      <p id='ubuntu-light' className="text-[12px] md:text-[12px]">8:30 am</p>
+                      <p id='ubuntu-light' className="text-[12px] md:text-[12px]">Hemos recibido tu pedido</p>
+                      <h2 id='ubuntu-medium' className='text-[12px] md:text-[12px]'>Sucursal Loma real</h2>
                     </div>
                   </div>
                 </div>
@@ -92,15 +92,15 @@ function EstatusDePedido() {
               </div>
               <div className="flex-1 text-center flex flex-col space-y-4">
                 <div className="flex flex-col items-center bg-white p-3 rounded-xl">
-                  <div className="flex flex-col space-y-4 mt-4 w-48 md:w-36 h-52 md:h-60">
-                    <div className="flex flex-col justify-between pb-2">
-                      <div className='flex items-center justify-center mb-4'>
-                        <img className='rounded-full w-20 md:w-15 h-20 md:h-20 object-cover' src="/icons/fastfood.svg" alt="En preparación" />
+                  <div className="flex flex-col space-y-4 mt-4 w-48 md:w-32 h-52 md:h-52 ">
+                    <div className="flex flex-col items-center justify-between pb-2">
+                      <div className='flex items-center justify-center rounded-full w-[100px] h-[100px] md:w-15 md:h-15 overflow-hidden'>
+                        <img className='w-auto h-auto max-w-full max-h-full' src="/icons/chef.svg" alt="En preparación" />
                       </div>
-                      <p className="font-semibold text-sm md:text-base">En preparación</p>
-                      <p className='text-black/40 text-sm md:text-base'>9:00 am</p>
-                      <p className="text-black/40 text-sm md:text-base">Estamos preparando tus productos</p>
-                      <h2 className='font-semibold text-sm md:text-base'>Sucursal Loma real</h2>
+                      <p id='ubuntu-medium' className="text-[16px] md:text-[16px]">En preparación</p>
+                      <p id='ubuntu-light' className="text-[12px] md:text-[12px]">9:00 am</p>
+                      <p id='ubuntu-light' className="text-[12px] md:text-[12px]">Estamos preparando tus productos</p>
+                      <h2 d='ubuntu-medium' className='text-[12px] md:text-[12px]'>Sucursal Loma real</h2>
                     </div>
                   </div>
                 </div>
@@ -111,15 +111,15 @@ function EstatusDePedido() {
               </div>
               <div className="flex-1 text-center flex flex-col space-y-4">
                 <div className="flex flex-col items-center bg-white p-3 rounded-xl">
-                  <div className="flex flex-col space-y-4 mt-4 w-48 md:w-36 h-52 md:h-60">
-                    <div className="flex flex-col justify-between ">
-                      <div className='flex items-center justify-center mb-4'>
-                        <img className='rounded-full w-20 md:w-15 h-20 md:h-20 object-cover' src="/icons/fastfood.svg" alt="En camino" />
+                  <div className="flex flex-col space-y-4 mt-4 w-48 md:w-32 h-52 md:h-52 ">
+                    <div className="flex flex-col items-center justify-between pb-2 ">
+                      <div className='flex items-center justify-center rounded-full w-[100px] h-[100px] md:w-15 md:h-15 overflow-hidden'>
+                        <img className='w-auto h-auto max-w-full max-h-full' src="/icons/deliver.svg" alt="En camino" />
                       </div>
-                      <p className="font-semibold text-sm md:text-base">En camino</p>
-                      <p className='text-black/40 text-sm md:text-base'>9:30 am</p>
-                      <p className="text-black/40 text-sm md:text-base">Tu pedido va en camino</p>
-                      <h2 className='font-semibold text-sm md:text-base'>Sucursal Loma real</h2>
+                      <p id='ubuntu-medium' className="text-[16px] md:text-[16px]">En camino</p>
+                      <p id='ubuntu-light' className="text-[12px] md:text-[12px]">9:30 am</p>
+                      <p id='ubuntu-light' className="text-[12px] md:text-[12px]">Tu pedido va en camino</p>
+                      <h2 d='ubuntu-medium' className='text-[12px] md:text-[12px]'>Sucursal Loma real</h2>
                     </div>
                   </div>
                 </div>
@@ -130,15 +130,15 @@ function EstatusDePedido() {
               </div>
               <div className="flex-1 text-center flex flex-col space-y-4">
                 <div className="flex flex-col items-center bg-white p-3 rounded-xl">
-                  <div className="flex flex-col space-y-4 mt-4 w-48 md:w-36 h-52 md:h-60">
-                    <div className="flex flex-col justify-between">
-                      <div className='flex items-center justify-center mb-4'>
-                        <img className='rounded-full w-20 md:w-15 h-20 md:h-20 object-cover' src="/icons/fastfood.svg" alt="Entregado" />
+                  <div className="flex flex-col space-y-4 mt-4 w-48 md:w-32 h-52 md:h-52 ">
+                    <div className="flex flex-col items-center justify-between pb-2">
+                      <div className='flex items-center justify-center  rounded-full w-[100px] h-[100px] md:w-15 md:h-15 overflow-hidden'>
+                        <img className='w-auto h-auto max-w-full max-h-full' src="/icons/home_svgrepo.com.svg" alt="Entregado" />
                       </div>
-                      <p className="font-semibold text-sm md:text-base">Entregado</p>
-                      <p className='text-black/40 text-sm md:text-base'>10:00 am</p>
-                      <p className="text-black/40 text-sm md:text-base">Tu pedido ha sido entregado en tu domicilio</p>
-                      <h2 className='font-semibold text-sm md:text-base'>Sucursal Loma real</h2>
+                      <p id='ubuntu-medium' className="text-[16px] md:text-[16px]">Entregado</p>
+                      <p id='ubuntu-light' className="text-[12px] md:text-[12px]">10:00 am</p>
+                      <p id='ubuntu-light' className="text-[12px] md:text-[12px]">Tu pedido ha sido entregado en tu domicilio</p>
+                      <h2 d='ubuntu-medium' className='text-[12px] md:text-[12px]'>Sucursal Loma real</h2>
                     </div>
                   </div>
                 </div>
