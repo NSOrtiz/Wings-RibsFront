5d5a246ce8b4766a8b7eeff1caa827e919cfaa35
Luis Medardo <soyriku@gmail.com>
2024-09-20 00:45:04 -0500

    se ajusta banner de promocion

--- src/components/BannerPromos.jsx
+++ src/components/BannerPromos.jsx
@@ -4,13 +4,20 @@ import bannerExample from '../assets/BannerExample.webp';
 export default function BannerPromo() {
   return (
     <div
-      className="my-6 w-full min-h-64 rounded-lg text-white text-center py-4 bg-gradient-to-r from-black via-[#f39c12] to-[#7b0000]"
+      className="my-6 w-full min-h-64 rounded-lg text-white text-center py-4 bg-gradient-to-r from-black via-[#f39c12] to-[#7b0000] flex items-center"
       style={{
         backgroundImage: `url(${bannerExample})`,
       }}
     >
-      <h2 className="text-lg font-bold">Lleva tus alitas con 20% descuento</h2>
-      <p>Válido para clientes con membresías</p>
+      <div className="flex-grow text-left p-4 ms-8">
+        <h2 className="text-2xl font-bold">
+          Lleva tus alitas con 20% descuento
+        </h2>
+        <p>Válido para clientes con membresías</p>
+      </div>
+      <div className="me-10">
+        <img src="/images/W&R2.png" alt="logo" className="w-[125px] h-[80px]" />
+      </div>
     </div>
   );
 }
