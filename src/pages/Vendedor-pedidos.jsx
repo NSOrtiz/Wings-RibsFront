956af8627c40c0c839011e7e5c481fa6d2355b3b
patrick espinoza <patrickespinoza38@gmail.com>
2024-10-03 22:11:05 -0600

    se agrego es componete de estatus de pedido para el boton de ver datalle y venderdor y administrador

--- src/pages/Vendedor-pedidos.jsx
+++ src/pages/Vendedor-pedidos.jsx
@@ -1,33 +1,16 @@
-import React, { useEffect, useState } from 'react';
 import { AdminSellerNavBar } from '../components/NavBar';
 import Footer from '../components/Footer';
+import React from 'react';
 import PedidoCard from '../components/PedidoCard';
 import { SellerMenu } from '../components/VerticalMenu';
 import { Search } from '../components/Button';
 
 export default function VendedorPedido() {
-  const [status, setStatus] = useState(null);
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-
-  const closeDropdown = () => {
-    setDropdownOpen(false);
-  };
-
-  const selectStatus = (newStatus) => {
-    setStatus(newStatus);
-    setDropdownOpen(false);
-  };
-
-  useEffect(() => {
-    window.addEventListener('click', closeDropdown);
-    return () => window.removeEventListener('click', closeDropdown);
-  }, []);
-
   return (
     <main className="w-full">
       <AdminSellerNavBar />
       <section className="w-full pl-0 pr-[16px] pb-[16px] md:pr-[56px] md:pb-[56px] lg:pb-[80px] lg:pr-[80px] flex flex-row gap-2">
-        <aside className="w-1/4 flex flex-col items-center hidden lg:block">
+        <aside className="w-1/4 flex flex-col items-center hidden lg:block  ">
           <SellerMenu />
         </aside>
 
@@ -40,24 +23,53 @@ export default function VendedorPedido() {
           </div>
 
           <div className="w-full flex justify-end items-center mb-10">
+            {/*<div className="flex space-x-4">
+              <div className="flex flex-row ">
+                <img src="/icons/ViewColumn.svg" alt="columna" />
+                <p className="text-[16px] text-amber-500 px-2">Columnas</p>
+              </div>
+              <div className="flex flex-row">
+                <img src="/icons/FilterList.svg" alt="filterList" />
+                <p className="text-[16px] text-amber-500 px-2">Filtros</p>
+              </div>
+              <div className="flex flex-row">
+                <img src="/icons/TableRows.svg" alt="tabalRows" />
+                <p className="text-[16px] text-amber-500 px-2">Densidad</p>
+              </div>
+              <div className="flex flex-row">
+                <img src="/icons/SaveAlt.svg" alt="saveAlt" />
+                <p className="text-[16px] text-amber-500 px-2">Exportar</p>
+              </div>
+            </div>*/}
             <Search />
           </div>
 
-          <div className="grid grid-cols-2 lg:grid-cols-6 sm:grid-cols-3 md:col-span-6 border-b border-neutral-500 mb-4">
-            <p className="flex-auto text-center text-[16px] text-amber-500">Detalles de pedido</p>
-            <p className="text-center text-[16px] text-amber-500">Tipo de pago</p>
-            <p className="text-center text-[16px] text-amber-500 hidden md:table-cell">Tiempo de preparación</p>
-            <p className="text-center text-[16px] text-amber-500 hidden md:table-cell">Estatus de pedido</p>
-            <p className="text-center text-[16px] text-amber-500 hidden md:table-cell">Hora aproximada de entrega</p>
-            <p className="text-center text-[16px] text-amber-500 hidden md:table-cell">Editar</p>
+          <div className="grid grid-cols-[1fr_1fr] md:grid-cols-[1fr_1fr_1fr] lg:grid-cols-[1fr_1fr_1fr_1fr]  2xl:grid-cols-[calc(22%+1px)_1fr_1fr_1fr_1fr_1fr] border-b border-neutral-500 mb-4 ">
+            <p className="flex flex-col mb-2 items-center justify-center text-[16px] text-amber-500">
+              Detalles de pedido
+            </p>
+            <p className="flex flex-col items-center justify-center text-[16px] text-amber-500">
+              Tipo de pago
+            </p>
+            <p className="lg:flex flex-col items-center justify-center  text-[16px] text-amber-500 hidden">
+              Tiempo de preparación
+            </p>
+            <p className="md:flex flex-col items-center justify-center text-[16px] text-amber-500 hidden ">
+              Estatus de pedido
+            </p>
+            <p className="2xl:flex flex-col items-center justify-center  text-[16px] text-amber-500 hidden ">
+              Hora aproximada de entrega
+            </p>
+            <p className="2xl:flex flex-col items-center justify-center text-[16px] text-amber-500 hidden ">
+              Editar
+            </p>
           </div>
-
           <PedidoCard />
           <PedidoCard />
           <PedidoCard />
           <PedidoCard />
           <PedidoCard />
-          <div className="flex justify-end"></div>
+          <div className="flex justify-end"> </div>
         </section>
       </section>
       <Footer />
